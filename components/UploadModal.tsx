0cd4fe2ea6ccb7a726c970ef9d65044f11c8b7d8
Amin Tuarshev <amin@Amins-MacBook-Pro.local>
2023-12-16 18:20:23 +0300

    Черновик проекта

---
+++ components/UploadModal.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import useUploadModal from "@/hooks/useUploadModal";
+import Modal from "./Modal";
+import { FieldValues, useForm } from "react-hook-form";
+
+const UploadModal = () => {
+  const uploadModal = useUploadModal();
+
+  const { register, handleSubmit, reset } = useForm<FieldValues>({
+    defaultValues: {
+      author: "",
+      title: "",
+      song: null,
+      image: null,
+    },
+  });
+
+  const onChange = (open: boolean) => {
+    if (!open) {
+      reset();
+      uploadModal.onClose();
+    }
+  };
+
+  const onSubmit = () => {
+    //Upload to supbase
+  }
+
+  return (
+    <Modal
+      title="Add a song"
+      description="Upload an mp3 file"
+      isOpen={uploadModal.isOpen}
+      onChange={onChange}
+    >
+      <form onSubmit={handleSubmit(onSubmit)}></form>
+    </Modal>
+  );
+};
+
+export default UploadModal;
